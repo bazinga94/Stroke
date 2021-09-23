de7ec718d7a68bdd97e27e1e378c81cd617e2c16
Jongho Lee <bazinga940@gmail.com>
2021-09-23 23:25:04 +0900

    For Haeyoon

--- Stroke WatchKit Extension/InterfaceController.swift
+++ Stroke WatchKit Extension/InterfaceController.swift
@@ -11,16 +11,32 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
-    override func awake(withContext context: Any?) {
-        // Configure interface objects here.
-    }
-    
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-    }
-    
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-    }
+	private let list: [String] = ["You", "You can", "You can make", "You can make it", "Baby", "Baby 😘"]
+	private var index: Int = 0
+
+	@IBOutlet weak var topTitleLabel: WKInterfaceLabel!
+
+	@IBAction func onTapButton() {
+		topTitleLabel.setText(list[index])
+		if index == list.count - 1 {
+			index = 0
+		} else {
+			index += 1
+		}
+	}
+
+
+
+	override func awake(withContext context: Any?) {
+		// Configure interface objects here.
+	}
+
+	override func willActivate() {
+		// This method is called when watch view controller is about to be visible to user
+	}
+
+	override func didDeactivate() {
+		// This method is called when watch view controller is no longer visible
+	}
 
 }
