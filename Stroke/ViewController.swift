966fd3273f9132c86e1997fa2b09648fea22d1d9
Jongho Lee <bazinga940@gmail.com>
2021-09-23 21:44:37 +0900

    First commit.

---
+++ Stroke/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stroke
+//
+//  Created by Jongho Lee on 2021/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
